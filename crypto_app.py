923bb3fbecc60da15f0b45fe4115026d79c90c15
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-12 21:56:27 -0700

    Update crypto_app.py

--- crypto_app.py
+++ crypto_app.py
@@ -5,47 +5,31 @@ def main():
     print("Welcome to the Crypto Information Portal!")
     coin_input = input("Enter the cryptocurrency symbol or name (e.g., BTC, Ethereum): ").strip()
 
-    coin_id = get_coin_id_by_symbol(coin_input)
+    coin_id = api_requests.get_coin_id_by_symbol(coin_input)
     if coin_id:
         try:
-            coin_details = get_cryptocurrency_details(coin_id)
+            coin_details = api_requests.get_cryptocurrency_details(coin_id)
             coin_name = coin_details['name']
-            coin_symbol = coin_details['symbol']
-            coin_description = coin_details['description']['en']
 
-            print("\nCryptocurrency Information:")
-            print(f"Name: {coin_name}")
-            print(f"Symbol: {coin_symbol}")
-            print(f"Description: {coin_description}\n")
+            print("\nChoose a currency to display prices in:")
+            available_currencies = ['usd', 'eur', 'gbp', 'jpy']
+            for idx, currency in enumerate(available_currencies, start=1):
+                print(f"{idx}. {currency.upper()}")
+            
+            currency_choice = int(input("Enter the number of your choice: ")) - 1
+            selected_currency = available_currencies[currency_choice]
 
-            currency_options = {
-                1: 'USD',
-                2: 'EUR',
-                3: 'GBP',
-                4: 'JPY'
-            }
+            # Fetch current price
+            price_data = api_requests.get_price_data([coin_id], selected_currency)
+            coin_price = price_data[coin_id][selected_currency]
+            print(f"Current price: {coin_price} {selected_currency.upper()}")
 
-            print("Choose a currency to display prices in:")
-            for num, currency in currency_options.items():
-                print(f"{num}. {currency}")
-
-            currency_choice = input("Enter the number of your choice: ")
-            chosen_currency = currency_options.get(int(currency_choice), 'USD')
-
-            # Count API calls
-            global api_call_count
-            api_call_count += 1
-
-            if api_call_count > MAX_API_CALLS:
-                print("Maximum number of API calls reached. Further data not available.")
-            else:
-                # Fetch current price
-                price_data = get_price_data([coin_id], chosen_currency)
-                coin_price = price_data[coin_id]
-                print(f"Current price: {coin_price} {chosen_currency}")
-
-        except requests.exceptions.HTTPError as http_err:
-            print(f"Error fetching data: {http_err}")
+            # Fetch and display historical data
+            historical_data = api_requests.get_historical_data(coin_id, 30)
+            chart_utils.display_historical_chart(historical_data, coin_name)
+            
+        except KeyError:
+            print("Data not available for the selected cryptocurrency.")
     else:
         print("Cryptocurrency not found.")
 
