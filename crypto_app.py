e86d2a7232902e3225a5532b7c0d647b31b3bf30
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-12 22:00:45 -0700

    Update crypto_app.py

--- crypto_app.py
+++ crypto_app.py
@@ -7,29 +7,32 @@ def main():
 
     coin_id = api_requests.get_coin_id_by_symbol(coin_input)
     if coin_id:
-        try:
-            coin_details = api_requests.get_cryptocurrency_details(coin_id)
-            coin_name = coin_details['name']
+        coin_details = api_requests.get_cryptocurrency_details(coin_id)
+        if coin_details:
+            try:
+                coin_name = coin_details['name']
 
-            print("\nChoose a currency to display prices in:")
-            available_currencies = ['usd', 'eur', 'gbp', 'jpy']
-            for idx, currency in enumerate(available_currencies, start=1):
-                print(f"{idx}. {currency.upper()}")
-            
-            currency_choice = int(input("Enter the number of your choice: ")) - 1
-            selected_currency = available_currencies[currency_choice]
+                print("\nChoose a currency to display prices in:")
+                available_currencies = ['usd', 'eur', 'gbp', 'jpy']
+                for idx, currency in enumerate(available_currencies, start=1):
+                    print(f"{idx}. {currency.upper()}")
+                
+                currency_choice = int(input("Enter the number of your choice: ")) - 1
+                selected_currency = available_currencies[currency_choice]
 
-            # Fetch current price
-            price_data = api_requests.get_price_data([coin_id], selected_currency)
-            coin_price = price_data[coin_id][selected_currency]
-            print(f"Current price: {coin_price} {selected_currency.upper()}")
+                # Fetch current price
+                price_data = api_requests.get_price_data([coin_id], 'usd', selected_currency)
+                if price_data:
+                    coin_price = price_data[coin_id]
+                    print(f"Current price: {coin_price} {selected_currency.upper()}")
 
-            # Fetch and display historical data
-            historical_data = api_requests.get_historical_data(coin_id, 30)
-            chart_utils.display_historical_chart(historical_data, coin_name)
-            
-        except KeyError:
-            print("Data not available for the selected cryptocurrency.")
+                    # Fetch and display historical data
+                    historical_data = api_requests.get_historical_data(coin_id, 30)
+                    if historical_data:
+                        chart_utils.display_historical_chart(historical_data, coin_name)
+                
+            except KeyError:
+                print("Data not available for the selected cryptocurrency.")
     else:
         print("Cryptocurrency not found.")
 
